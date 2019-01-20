3df35ea37a0b1dabfaa147ebb4e5651339685f44
Anton <a.averianov@gmail.com>
2019-01-20 18:46:46 +0700

    Добавили класс Figure

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,17 +10,20 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetWindowSize(1, 1);
+            Console.SetBufferSize(80, 25);
+            Console.SetWindowSize(80, 25);
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            //Рисуем рамочку
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
 
-            HorizontalLine line1 = new HorizontalLine(5, 10, 8, '+');
-            line1.Draw();
-
-            VerticalLine line2 = new VerticalLine(5, 10, 8, '*');
-            line2.Draw();
             Console.ReadKey();
         }
     }
